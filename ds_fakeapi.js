0c5790c7149f356be88f513a3d06de281f81849f
Rodrigo Moraes <rodrigo.moraes2012@gmail.com>
2018-01-02 16:53:03 -0200

    Requisição GET sem autenticação

--- ds_fakeapi.js
+++ ds_fakeapi.js
@@ -2,47 +2,48 @@ function defineStructure(){}
 function onSync(lastSyncDate){}
 function onMobileSync(user){}
 function createDataset(fields, constraints, sortFields) {
+	
 	var dataset = DatasetBuilder.newDataset();
-	var vo = null;
 	
-	// REST
+	//Cria as colunas
+		dataset.addColumn("ID");
+		dataset.addColumn("User Name");
+		dataset.addColumn("Password");
+	
 	try{
-        var clientService = fluigAPI.getAuthorizeClientService();
+		// REST
+        	var clientService = fluigAPI.getAuthorizeClientService();
         
-        var data = {
-            companyId : getValue("WKCompany") + '',
-            serviceCode : 'FakeAPI',
-            endpoint : '/api/users',
-            method : 'get',
-            timeoutService: '15' // segundos
-        }
-        
-        vo = clientService.invoke(JSON.stringify(data));
- 
-        if(vo.getResult()== null || vo.getResult().isEmpty()){
-            throw new Exception("Retorno está vazio");
-        }else{
-            log.info(vo.getResult());
-        }
-        
-        var jsonParse = JSON.parse(vo.getResult());
-        
-        //Cria as colunas
-        dataset.addColumn("ID");
-        dataset.addColumn("User Name");
-        dataset.addColumn("Password");
-        
-        //Cria os registros
-        for(var i=0; i<jsonParse.length; i++){
-        	dataset.addRow(new Array(jsonParse[i].ID, 
-        			jsonParse[i].UserName, 
-        			jsonParse[i].Password));
-        }
-        
-    } catch(err) {
-        throw new Exception(err);
-    }
-	
-    return dataset;
+		// Parametros
+		var data = {
+		    companyId : getValue("WKCompany") + '',
+		    serviceCode : 'FakeAPI', // Campo Nome no cadastro do serviço
+		    endpoint : '/api/users',
+		    method : 'get',
+		    timeoutService: '10' // segundos
+		}
 
+		// Dispara requisição GET para http://fakerestapi.azurewebsites.net/api/users
+		var vo = clientService.invoke(JSON.stringify(data));
+
+		if(vo.getResult()== null || vo.getResult().isEmpty()){
+		    throw new Exception("Retorno está vazio");
+		}else{
+		    log.info(vo.getResult());
+		}
+
+		var jsonParse = JSON.parse(vo.getResult());
+
+		//Cria os registros para o Dataset
+		for(var i=0; i<jsonParse.length; i++){
+			dataset.addRow(new Array(jsonParse[i].ID, 
+					jsonParse[i].UserName, 
+					jsonParse[i].Password));
+		}
+
+	} catch(err) {
+		throw new Exception(err);
+	}
+	
+    	return dataset;
 }
