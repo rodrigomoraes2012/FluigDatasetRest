c0d36620d08d35cf232da59c2770746163124946
Rodrigo Moraes <rodrigo.moraes2012@gmail.com>
2018-01-02 16:55:36 -0200

    Requisição REST método GET simples

--- ds_fakeapi.js
+++ ds_fakeapi.js
@@ -6,9 +6,9 @@ function createDataset(fields, constraints, sortFields) {
 	var dataset = DatasetBuilder.newDataset();
 	
 	//Cria as colunas
-		dataset.addColumn("ID");
-		dataset.addColumn("User Name");
-		dataset.addColumn("Password");
+	dataset.addColumn("ID");
+	dataset.addColumn("User Name");
+	dataset.addColumn("Password");
 	
 	try{
 		// REST
@@ -16,20 +16,20 @@ function createDataset(fields, constraints, sortFields) {
         
 		// Parametros
 		var data = {
-		    companyId : getValue("WKCompany") + '',
-		    serviceCode : 'FakeAPI', // Campo Nome no cadastro do serviço
-		    endpoint : '/api/users',
-		    method : 'get',
-		    timeoutService: '10' // segundos
+			companyId : getValue("WKCompany") + '',
+			serviceCode : 'FakeAPI', // Campo Nome no cadastro do serviço
+			endpoint : '/api/users',
+			method : 'get',
+			timeoutService: '10' // segundos
 		}
 
 		// Dispara requisição GET para http://fakerestapi.azurewebsites.net/api/users
 		var vo = clientService.invoke(JSON.stringify(data));
 
 		if(vo.getResult()== null || vo.getResult().isEmpty()){
-		    throw new Exception("Retorno está vazio");
+			throw new Exception("Retorno está vazio");
 		}else{
-		    log.info(vo.getResult());
+			log.info(vo.getResult());
 		}
 
 		var jsonParse = JSON.parse(vo.getResult());
